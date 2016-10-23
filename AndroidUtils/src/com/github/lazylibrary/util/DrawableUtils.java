cc55a8f13737855b4dc656d4611c2fa4d3ff2a67
q296494327 <296494327@qq.com>
2016-10-23 19:31:24 +0800

    增加了drawable图片缩放工具

--- AndroidUtils/src/com/github/lazylibrary/util/DrawableUtils.java
+++ AndroidUtils/src/com/github/lazylibrary/util/DrawableUtils.java
@@ -55,6 +55,45 @@ public class DrawableUtils {
 	StateListDrawable selector = getStateListDrawable(bgNormal, bgPress);
 	return selector;
     }
+
+
+
+	 /**
+     * drawable图片缩放工具
+     * @param drawable
+     * @param w
+     * @param h
+     * @return
+     */
+    public static Drawable zoomDrawable(Drawable drawable, int w, int h) {
+        int width = drawable.getIntrinsicWidth();
+        int height = drawable.getIntrinsicHeight();
+        Bitmap oldbmp = drawableToBitmap(drawable);
+        Matrix matrix = new Matrix();
+        float scaleWidth = ((float) w / width);
+        float scaleHeight = ((float) h / height);
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap newbmp = Bitmap.createBitmap(oldbmp, 0, 0, width, height,
+                matrix, true);
+        return new BitmapDrawable(null, newbmp);
+    }
+
+    /**
+     * drawable转成bitmap
+     * @param drawable
+     * @return
+     */
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        int width = drawable.getIntrinsicWidth();
+        int height = drawable.getIntrinsicHeight();
+        Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                : Bitmap.Config.RGB_565;
+        Bitmap bitmap = Bitmap.createBitmap(width, height, config);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, width, height);
+        drawable.draw(canvas);
+        return bitmap;
+    }
     
     
 }
